3fc80037bf18539c21854755efd075fdffd14779
contarna <lipengsong@hotmail.com>
2017-01-12 00:04:20 +0800

    2017-1-12 1:实现MySQL数据库连接和SQL语句执行 2:实现查询数据并显示在表格中

--- WPF-MySQL/MainWindow.xaml.cs
+++ WPF-MySQL/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace WPF_MySQL
 {
@@ -20,9 +21,24 @@ namespace WPF_MySQL
     /// </summary>
     public partial class MainWindow : Window
     {
+        MySQLOperate mysql_Operate = new MySQLOperate();
+
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
+            mysql_Operate.DatabaseConnect();
+        }
+
+        private void Serach(object sender, RoutedEventArgs e)
+        {
+            mysql_Operate.SqlCmd = "select name,realname,sex from user";
+
+            //MySqlCommand cmd = new MySqlCommand(SqlCmd, MySqlInstance);
+            //MySqlDataAdapter ada = new MySqlDataAdapter(cmd);
+            //DataSet ds = new DataSet();
+            //ada.Fill(ds);//查询结果填充数据集
+            DataSet data=mysql_Operate.DatabaseExcute();
+            dataGrid.ItemsSource = data.Tables[0].DefaultView;
         }
     }
 }
--- WPF-MySQL/MySQLOperate.cs
+++ WPF-MySQL/MySQLOperate.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace WPF_MySQL
 {
@@ -13,7 +14,25 @@ namespace WPF_MySQL
     /// </summary>
     class MySQLOperate
     {
-        
+        public MySqlConnection MySqlInstance;
+        public string SqlCmd;
+
+        //连接数据库
+        public void DatabaseConnect()
+        {
+            string str = "Server=127.0.0.1;User ID=root;Password=wen1229;Database=project;CharSet=gbk;";
+            MySqlInstance = new MySqlConnection(str);//实例化链接
+            MySqlInstance.Open();//开启连接
+        }
+
+        public DataSet DatabaseExcute()
+        {
+            MySqlCommand cmd = new MySqlCommand(SqlCmd, MySqlInstance);
+            MySqlDataAdapter ada = new MySqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            ada.Fill(ds);//查询结果填充数据集
+            return ds;
+        }
 
     }
 }
