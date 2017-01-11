eb52c4f9240144e9b801c9aac109acfa78ad8576
contarna <lipengsong@hotmail.com>
2017-01-11 23:08:22 +0800

    添加项目文件。

---
+++ WPF-MySQL/MySQLOperate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+
+namespace WPF_MySQL
+{
+    /// <summary>
+    /// MySQL 数据库操作类
+    /// </summary>
+    class MySQLOperate
+    {
+        
+
+    }
+}
